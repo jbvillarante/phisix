0ff2e1717995bac81c034195baa44cca68b4e297
Jay Ricky Villarante <jbvillarante@gmail.com>
2011-08-15 21:12:48 +0800

    commit phisix app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phisix::Application.config.secret_token = '9eb557642565bb2e05eecebe8d6108f79e9a31ede9a11a5965798fba0d52fa1353970e7bd43187a118dc5603820f6b48cdced122a1000e090d089ade6be1baa5'
